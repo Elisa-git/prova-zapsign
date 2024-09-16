8c4eccb29e2b00fb874b4fc8da3da8c3daa109e6
Elisa Mesquita <62437484+Elisa-git@users.noreply.github.com>
2024-09-16 12:47:22 -0300

    Teste unitario model signer e document

--- Back/Zapi/tests/test_models.py
+++ Back/Zapi/tests/test_models.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from Zapi.models import Company
+from Zapi.models import Company, Document, Signer
 import datetime
 from django.conf import settings
 
@@ -23,5 +23,75 @@ class ModelCompanyTestCase(TestCase):
         self.assertEqual(self.company.api_token, company_compare['api_token'])
 
 
-# class ModelDocumentTestCase(TestCase):
-# class ModelSignerTestCase(TestCase):
+class ModelDocumentTestCase(TestCase):
+    def setUp(self):
+
+        self.company = Company.objects.create(
+            name = 'Empresa company'
+        )
+
+        self.document = Document.objects.create(
+            name = 'Documento Um',
+            status = 'Novo',
+            created_by = 'Fulano de Tal',
+            companyId = self.company,
+            openId = 123
+        )
+
+    def test_verifica_atributos_document(self):
+        document_compare = {
+            'name': 'Documento Um',
+            'status': 'Novo',
+            'created_by': 'Fulano de Tal',
+            'companyId': self.company,
+            'openId': 123
+        }
+
+        self.assertEqual(self.document.name, document_compare['name'])
+        self.assertEqual(self.document.status, document_compare['status'])
+        self.assertEqual(self.document.created_by, document_compare['created_by'])
+        self.assertEqual(self.document.companyId, document_compare['companyId'])
+        self.assertEqual(self.document.openId, document_compare['openId'])
+
+class ModelSignerTestCase(TestCase):
+    def setUp(self):
+        self.company = Company.objects.create(
+            name = 'Empresa company'
+        )
+                
+        self.document = Document.objects.create(
+            name = 'Documento Um',
+            status = 'Novo',
+            created_by = 'Fulano de Tal',
+            companyId = self.company,
+            openId = 123
+        )
+                
+        self.signer = Signer.objects.create(
+            token = '123',
+            status = 'Novo',
+            name = 'Fulano',
+            email = 'fula@email.com',
+            externalId = settings.API_TOKEN,
+            documentId = self.document
+        )
+    
+    def test_verifica_atributos_signer(self):
+        signer_compare = {
+            'token': '123',
+            'status': 'Novo',
+            'name': 'Fulano',
+            'email': 'fula@email.com',
+            'externalId': settings.API_TOKEN,
+            'documentId': self.document
+        }
+
+        self.assertEqual(self.signer.token, signer_compare['token'])
+        self.assertEqual(self.signer.status, signer_compare['status'])
+        self.assertEqual(self.signer.name, signer_compare['name'])
+        self.assertEqual(self.signer.email, signer_compare['email'])
+        self.assertEqual(self.signer.externalId, signer_compare['externalId'])
+        self.assertEqual(self.signer.documentId, signer_compare['documentId'])
+
+
+
